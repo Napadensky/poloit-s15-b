1ae556c94a91a0b2225d3ffa7c71a155627c5cf9
Floh2023 <florencia.marcazzo@gmail.com>
2024-08-26 19:27:09 -0300

    feat/ crud para tags

--- src/server.js
+++ src/server.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 const connectDB = require('./config/db'); // Importa connectDB correctamente
 const userRoutes = require('./routes/userRoutes');
+const tagRoutes = require('./routes/tagRoutes')
 
 connectDB();
 
 app.use(express.json()); // Para parsear JSON en el cuerpo de las solicitudes
 app.use('/api', userRoutes); // Rutas de usuarios
+app.use('/api', tagRoutes)
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
