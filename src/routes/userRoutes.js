1186c48060167f0ffb8e5a2ba50741a58f564c58
Floh2023 <florencia.marcazzo@gmail.com>
2024-08-30 17:22:29 -0300

    "FEAT/instale express validator y cree middleware"

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const validateUser = require('../middleware/validateUser');
 
 // Crear un nuevo usuario
-router.post('/users', userController.createUser);
+router.post('/users',validateUser, userController.createUser);
 
 // Leer todos los usuarios
 router.get('/users', userController.getAllUsers);
