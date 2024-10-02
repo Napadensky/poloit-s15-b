375d097de44d6b0f6a92b1e84505c194aefba7ca
Floh2023 <florencia.marcazzo@gmail.com>
2024-10-02 18:45:30 -0300

    FEAT/ ACT

--- src/models/Enrolled.js
+++ src/models/Enrolled.js
@@ -5,11 +5,12 @@ const enrolledSchema = new mongoose.Schema({
   dni: { type: String, required: true },
   mail: { type: String, required: true },
   phone: { type: String, required: true },
-  active: { type: Boolean, required: true },
+  active: { type: Boolean, required: true, default: false },
   role: { type: String, enum: ['QA', 'UX/UI', 'Frontend', 'Backend'], required: true },
   projects: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Project' }], // Campo para asociar los cursos
-  assigned: { type: Boolean, default: false }  // Campo para marcar si ya está asignado a un squad
-
+  assigned: { type: Boolean, default: false },
+  ong:{ type: String, required: true },
+  link:{ type: String, required: true },
 });
 
 const Enrolled = mongoose.model('Enrolled', enrolledSchema);
